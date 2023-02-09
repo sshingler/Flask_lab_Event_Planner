f70306ba8e53579cdd2eba61608e6dc082be2512
SShingler <s.shingler@hotmail.co.uk>
2023-02-09 16:09:58 +0000

    2 - Flask lab - Event Planner - MVP + extensions complete

--- models/event_list.py
+++ models/event_list.py
@@ -3,8 +3,8 @@ import datetime
 
 
 
-event_1 = Event(datetime.date(2023, 2, 8), "Tech-Meetup-Lochaber", 20, "The Grog & Gruel", "A meet up for tech enthusiasts in Lochaber - Grog & Gruel pub.")
-event_2 = Event(datetime.date(2023, 2, 7), "Tech-Meetup-Argyll", 20, "The Oban Inn", "A meet up for tech enthusiasts in Argyll - The Oban Inn.")
+event_1 = Event(datetime.date(2023, 2, 8), "Tech-Meetup-Lochaber", 20, "The Grog & Gruel", "A meet up for tech enthusiasts in Lochaber")
+event_2 = Event(datetime.date(2023, 2, 7), "Tech-Meetup-Argyll", 20, "The Oban Inn", "A meet up for tech enthusiasts in Argyll")
 events_list = [event_1, event_2]
 
 def add_new_event(event):
